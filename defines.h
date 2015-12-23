ac28d73e8b6a20756e56c7af89b32d42144c4156
Manuel Wurth <stigaccs@mail.com>
2015-12-23 08:29:25 +0100

    add missing difines.h file

---
+++ defines.h
@@ -0,0 +1,32 @@
+/*
+    PROGRAM DEFINES
+*/
+#ifdef ON_TARGET
+    #define TRAVERSE_TIME_ONE_TM                        1500
+    #define RUN_LONGER_THAN_THEORETICALLY_NEEDED        500
+    #define EMPTY_SPACE_TO_BE_READY                     1200
+    #define TRAVERSE_TIME_AFTER_CENTERED_LB             50
+    #define TOOL_TIME                                   3000
+    #define TIMEOUT                                     7000
+    #define STAGE_SIX_LB_CHECK_FULL_TIME                3000
+    #define STAGE_SIX_CHECK_INTERVAL                    3000
+    #define STAGE_SIX_RUN_TM_TOO_CHECK_IF_ITEM_WAS_REMOVED_TIME     2000
+#else
+
+    #define TRAVERSE_TIME_ONE_TM                        5000
+    #define RUN_LONGER_THAN_THEORETICALLY_NEEDED        500
+    #define EMPTY_SPACE_TO_BE_READY                     1200
+    #define TRAVERSE_TIME_AFTER_CENTERED_LB             700
+    #define TOOL_TIME                                   3000
+    #define TIMEOUT                                     15000 //from LB to LB
+    #define STAGE_SIX_LB_CHECK_FULL_TIME                3000
+    #define STAGE_SIX_CHECK_INTERVAL                    3000
+    #define STAGE_SIX_RUN_TM_TOO_CHECK_IF_ITEM_WAS_REMOVED_TIME     2000
+#endif
+
+/*
+    SIMULATOR DEFINES
+*/
+
+#define SLEEP_TIME 10
+#define PUSHER_SPEED_PER_MS 0.0002
